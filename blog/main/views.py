2984d55a59d1901decf737d87d6184c23914834e
Aaron-lab- c <aaron0321sy@gmail.com>
2019-10-25 16:26:28 +0800

    main/view.py main/main/main.html main/main/about.html update

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
-
 def main(request):
     '''
-    Show 'Hello world!' in the main page
+    Render the main page
     '''
     context = {'like':'Django 很棒'}
     return render(request, 'main/main.html', context)
+
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
